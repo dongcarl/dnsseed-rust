804547d20b708226f68d199819ac537ed2e69dc2
Matt Corallo <git@bluematt.me>
2019-05-20 22:24:09 -0400

    Also print hours for scan times

--- src/printer.rs
+++ src/printer.rs
@@ -76,9 +76,10 @@ impl Printer {
 
 				out.write_all(b"\nRetry times (in seconds):\n").expect("stdout broken?");
 				for i in 0..AddressState::get_count() {
+					let scan_secs = store.get_u64(U64Setting::RescanInterval(AddressState::from_num(i).unwrap()));
 					out.write_all(format!(
-							"{:22} ({:2}): {}\n", AddressState::from_num(i).unwrap().to_str(), i,
-							store.get_u64(U64Setting::RescanInterval(AddressState::from_num(i).unwrap()))
+							"{:22} ({:2}): {:5} (ie {} min, {} hrs)\n", AddressState::from_num(i).unwrap().to_str(), i,
+							scan_secs, scan_secs / 60, scan_secs / 60 / 60,
 							).as_bytes()).expect("stdout broken?");
 				}
 
